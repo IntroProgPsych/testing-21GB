68ba23c0bff2d0f452fbabf29e0c77a6a46af0b6
21GB <ioan.brinzei01@e-uvt.ro>
2025-12-18 10:38:19 +0000

    bomboclat

--- Part2_Exercise2/2.2_Test_IsEven.py
+++ Part2_Exercise2/2.2_Test_IsEven.py
@@ -13,7 +13,18 @@ class TestIsEven(unittest.TestCase):
     # - Use clear method names, e.g. test_even_positive_number, etc.
     #
     # write your tests here
-    pass
+
+    def test_even_positive_number(self):
+        self.assertTrue(is_even(2))
+
+    def test_even_zero(self):
+        self.assertTrue(is_even(0))
+
+    def test_odd_number(self):
+        self.assertFalse(is_even(7))
+
+    def test_even_negative_number(self):
+        self.assertTrue(is_even(-4))
 
 
 if __name__ == "__main__":
